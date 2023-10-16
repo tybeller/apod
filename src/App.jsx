642b457a8bb5f6f0ffbe0dd110eb4cc0bbf43343
tybeller <ty.c.beller@gmail.com>
2023-10-16 00:10:48 -0400

    working ban list

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,7 @@ const API_KEY = import.meta.env.VITE_CAT_API_KEY
 function App() {
 
   const [nasaData, setNasaData] = useState(null)
-  const [banList, setBanList] = useState(
-    {
-      breeds: [],
-      origin: [],
-      weight: [],
-    }
-  )
+  const [banList, setBanList] = useState([])
 
   /*useEffect(() => {
     async function fetchNasaData() {
@@ -35,28 +29,23 @@ function App() {
   const getNewImage = async () => {
     const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=${API_KEY}`)
     const data = await res.json()
+
+    if (banList.includes(data[0].breeds[0].name) || banList.includes(data[0].breeds[0].origin) || banList.includes(data[0].breeds[0].weight.imperial)) {
+      getNewImage()
+      return
+    }
     setNasaData(data)
+    console.log(data)
   }
 
-  const banBreedName = (breedName) => {
-    setBanList({
-      ...banList,
-      breeds: [...banList.breeds, breedName]
-    })
+  const banItem = (item) => {
+    if (!banList.includes(item)) {
+      setBanList([...banList, item])
+    }
   }
 
-  const banOrigin = (origin) => {
-    setBanList({
-      ...banList,
-      origin: [...banList.origin, origin]
-    })
-  }
-  
-  const banWeight = (weight) => {
-    setBanList({
-      ...banList,
-      weight: [...banList.weight, weight]
-    })
+  const unbanItem = (item) => {
+    setBanList(banList.filter((d) => d !== item))
   }
 
   return (
@@ -67,23 +56,32 @@ function App() {
 
         {nasaData 
         ? 
-        <div className='thing-holder'>
-          <div className='attr-holder'>
-            <button>{nasaData[0].breeds[0].name}</button>
-            <button>{nasaData[0].breeds[0].origin}</button>
-            <button>{nasaData[0].breeds[0].weight.imperial} lbs</button>    
+          <div className='thing-holder'>
+            <div className='attr-holder'>
+              <button onClick={() => banItem(nasaData[0].breeds[0].name)}>{nasaData[0].breeds[0].name}</button>
+              <button onClick={() => banItem(nasaData[0].breeds[0].origin)}>{nasaData[0].breeds[0].origin}</button>
+              <button onClick={() => banItem(nasaData[0].breeds[0].weight.imperial)}>{nasaData[0].breeds[0].weight.imperial} lbs</button>    
+            </div>
+            <img className='apod-image' src={nasaData[0].url} />
           </div>
-          <img className='apod-image' src={nasaData[0].url} />
-        </div>
         : <></>
         }
 
         <button onClick={getNewImage}>Get New Image</button>
+
+        <div className='thing-holder ban'>
+          <h2>Ban List</h2>
+          <div className='attr-holder'>
+            {Object.entries(banList).map((d, i) => (
+                <button key={i} onClick={() => unbanItem(d[1])}>
+                  {d[1]}
+                </button>
+            ))}
+          </div>
+        </div>
       </div>
+
       
-      <div>
-        banList: {JSON.stringify(banList)}
-      </div>
     </>
   )
 }
